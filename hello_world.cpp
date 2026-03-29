a1d0e3595a2d50a5bffd11eaa7120ab9134e8f33
Arseniy <aquaqew@mail.ru>
2026-03-29 15:10:45 -0500

    add comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 
+// main function
 int main() {
+    // creating name
     std::string name;
+    // request name
     std::cout << "Enter your name: ";
+    // read name
     std::cin >> name;
-    std::cout << "Hello world from " << name << std::endl;
+    // print greeting
+    std::cout << "Hello World from " << name << std::endl;
     return 0;
 }
