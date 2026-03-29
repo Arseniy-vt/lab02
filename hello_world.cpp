65783ffeb5d1e7de8c7c2b470e15971ff34eec09
Арсений <151772861+Arseniy-vt@users.noreply.github.com>
2026-03-29 23:19:56 +0300

    Revert "Patch1"

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
 
-// main function
+using namespace std;
+
 int main() {
-    // creating name
-    std::string name;
-    // request name
-    std::cout << "Enter your name: ";
-    // read name
-    std::cin >> name;
-    // print greeting
-    std::cout << "Hello World from " << name << std::endl;
+    string name;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Hello world from " << name << endl;
     return 0;
 }
