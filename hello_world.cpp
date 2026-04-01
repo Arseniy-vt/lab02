8e18feac296298164743deb790f881eec1b20310
Arseniy <aquaqew@mail.ru>
2026-04-01 08:47:25 -0500

    changed code style

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 // main function
-int main() {
-    // user name
-    std::string name;
-    // request for name
-    std::cout << "Enter your name: ";
-    // read name
-    std::cin >> name;
-    // print greeting
-    std::cout << "Hello World from " << name << std::endl;
-    return 0;
+int
+main()
+{
+  // user name
+  std::string name;
+  // request for name
+  std::cout << "Enter your name: ";
+  // read name
+  std::cin >> name;
+  // print greeting
+  std::cout << "Hello World from " << name << std::endl;
+  return 0;
 }
